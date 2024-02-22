c02d0f5a1ef7d2dbbcc51cc87d21ce7685b9746f
Estebanpt12 <esteban.penap@uqvirtual.edu.co>
2024-02-22 16:34:39 -0500

    Implementacion clase Conjunto

--- src/modelo/Conjunto.py
+++ src/modelo/Conjunto.py
@@ -1,3 +1,35 @@
 class Conjunto:
     def __init__(self, elementos):
         self.elementos = elementos
+
+    def union(self, otro_conjunto):
+        resultado = self.elementos.copy()
+        for elemento in otro_conjunto.elementos:
+            if elemento not in resultado:
+                resultado.append(elemento)
+        return Conjunto(resultado)
+
+    def interseccion(self, otro_conjunto):
+        resultado = []
+        for elemento in self.elementos:
+            if elemento in otro_conjunto.elementos:
+                resultado.append(elemento)
+        return Conjunto(resultado)
+
+    def diferencia(self, otro_conjunto):
+        resultado = []
+        for elemento in self.elementos:
+            if elemento not in otro_conjunto.elementos:
+                resultado.append(elemento)
+        return Conjunto(resultado)
+
+    def complemento(self, *otros_conjuntos):
+        resultado = []
+        for otro_conjunto in otros_conjuntos:
+            for elemento in otro_conjunto.elementos:
+                if elemento not in self.elementos and elemento not in resultado:
+                    resultado.append(elemento)
+        return Conjunto(resultado)
+
+    def __str__(self):
+        return str(self.elementos)
